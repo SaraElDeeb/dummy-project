cf485d3196a17a53ad54a295157d4e8e84ddc199
SaraElDeeb <saraadel.fcis2012>
2015-06-17 23:42:11 +0100

    bubble sort

---
+++ dummy project/dummy project/Sort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dummy_project
+{
+    class Sort
+    {
+
+        public int[] bubble_sort(int[] digits)
+        {
+            int temp;
+            int number_of_digits = digits.Length - 1;
+            
+            for (int i = number_of_digits; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (digits[j] > digits[j + 1])
+                    {
+                        temp = digits[j];
+                        digits[j] = digits[j + 1];
+                        digits[j + 1] = temp;
+                    }
+                }
+            }
+
+            return digits;
+        }
+    }
+}
