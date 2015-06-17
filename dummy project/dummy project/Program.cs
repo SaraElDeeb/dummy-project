5f8bb34755e22b53dfa11c26f906d3fdcb0757f7
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-06-18 01:53:51 +0200

    re-construct Program.cs to print the two sort algorithms

--- dummy project/dummy project/Program.cs
+++ dummy project/dummy project/Program.cs
@@ -11,12 +11,22 @@ namespace dummy_project
             int[] Digits = { -2, 45, 0, 11, -9 };
 
             Sort s = new Sort();
-
-            Digits = s.bubble_sort(Digits);
+            int[] sorted_list = s.bubble_sort(Digits);
+			Console.WriteLine ("Sorted by Bubble Sort");
             for (int k = 0; k < Digits.Length; k++)
             {
-                Console.WriteLine(Digits[k]);
+				Console.WriteLine(sorted_list[k]);
             }
+
+			Console.WriteLine ();
+
+			InsertionSort insertion_sort = new InsertionSort ();
+			sorted_list = insertion_sort.Sort(Digits);
+			Console.WriteLine ("Sorted by Insertion Sort");
+			for (int k = 0; k < Digits.Length; k++)
+			{
+				Console.WriteLine(sorted_list[k]);
+			}
         }
     }
 }
