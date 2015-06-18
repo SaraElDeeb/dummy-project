ed1de0df53f09456ee0faaa5b1f17207700dc85f
SaraElDeeb <saraadel.fcis2012>
2015-06-18 15:35:57 +0100

    intiate dummy window

---
+++ dummy project/dummy project/dummy window.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dummy_project
+{
+    public partial class dummy_window : Form
+    {
+        public dummy_window()
+        {
+            InitializeComponent();
+        }
+    }
+}
